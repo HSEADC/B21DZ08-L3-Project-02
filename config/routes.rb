2c14ed7304d48170f524c391ceaa338cc0bcceec
Polina Barybina <90695326+polirybka@users.noreply.github.com>
2023-10-05 23:44:37 +0300

    we tried

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'diary/index'
+  get 'profile/index'
+  resources :comments
+  resources :posts
+  get 'welcome/index'
+  get 'welcome/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
 end
