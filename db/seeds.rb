82b138a7dbb5bde5b3866a8b69acc8eed872562c
KlimovaDasha <89785723+KlimovaDasha@users.noreply.github.com>
2023-10-08 21:19:34 +0300

    Second tutorial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+@raw_text = 'Дом Наркомфина — один из знаковых памятников архитектуры советского авангарда и конструктивизма. Построен в 1928—1930 годах по проекту архитекторов Моисея Гинзбурга, Игнатия Милиниса и инженера Сергея Прохорова для работников Народного комиссариата финансов СССР (Наркомфина). Автор замысла дома Наркомфина Гинзбург определял его как «опытный дом переходного типа». Дом находится в Москве по адресу: Новинский бульвар, дом 25, корпус 1. С начала 1990-х годов дом находился в аварийном состоянии, был трижды включён в список «100 главных зданий мира, которым грозит уничтожение». В 2017—2020 годах отреставрирован по проекту АБ «Гинзбург Архитектс», функционирует как элитный жилой дом. Отдельно стоящий «Коммунальный блок» (историческое название) планируется как место проведения публичных мероприятий.'
+@words = @raw_text.downcase.gsub(/[—.—,«»:()]/, '').gsub(/  /, ' ').split(' ')
+
+def seed
+  reset_db
+  create_plants(10)
+  create_comments(2..8)
+end
+
+def reset_db
+  Rake::Task['db:drop'].invoke
+  Rake::Task['db:create'].invoke
+  Rake::Task['db:migrate'].invoke
+end
+
+def create_sentence
+  sentence_words = []
+
+  (10..20).to_a.sample.times do
+    sentence_words << @words.sample
+  end
+
+  sentence = sentence_words.join(' ').capitalize + '.'
+end
+
+def create_plants(quantity)
+  quantity.times do
+    plant = Plant.create(name: create_sentence, description: create_sentence)
+    puts "Plant with id #{plant.id} just created"
+  end
+end
+
+def create_comments(quantity)
+  Plant.all.each do |plant|
+    quantity.to_a.sample.times do
+      comment = Comment.create(plant_id: plant.id, body: create_sentence)
+      puts "Comment with id #{comment.id} for plant with id #{comment.plant.id} just created"
+    end
+  end
+end
+
+seed
